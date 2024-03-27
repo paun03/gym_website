45d521c4c8f02d904fa223f75a8804501b23a5a1
paun03 <paunovicm2003@gmail.com>
2024-03-27 16:09:01 +0100

    fixing minor design issues

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import "./Hero.css";
+
+const Hero = () => {
+    return (
+        <main>
+            <div>
+                <h1>Free Trial Session With<br></br>
+                A Trainer!</h1>
+                <button>Detailed</button>
+            </div>
+        </main>
+    )
+}
+
+export default Hero;
